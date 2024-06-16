3881c5fb3b40982ad232b5efaab51d6187ba2ea3
HaniBassam <143654847+HaniBassam@users.noreply.github.com>
2024-06-17 00:46:12 +0200

    update

---
+++ js/bearedygtighed.js
@@ -0,0 +1,45 @@
+// Get the shop element
+const shopElement = document.getElementById("shop");
+
+const omOsElement = document.getElementById("omOs");
+
+// Get the shop nav container
+const shopNavContainer = document.getElementById("shop-nav-container");
+// get the om os nav container
+const omOsNavContainer = document.getElementById("om-os-nav-container");
+
+// hiding containers
+shopNavContainer.style.display = "none";
+omOsNavContainer.style.display = "none";
+
+// Add event listener for mouseover event on shop element or nav container
+shopElement.addEventListener("mouseover", () => {
+  // Show the shop nav container
+  shopNavContainer.style.display = "block";
+  shopNavContainer.style.display = "flex";
+  // Hide the omOs nav container
+  omOsNavContainer.style.display = "none";
+});
+shopNavContainer.addEventListener("mouseleave", () => {
+  // Hide the shop nav container
+  shopNavContainer.style.display = "none";
+});
+
+// Add event listener for mouseover event on omOs element or om os nav container
+omOsElement.addEventListener("mouseover", () => {
+  // Show the omOs nav container
+  omOsNavContainer.style.display = "block";
+  // Hide the shop nav container
+  shopNavContainer.style.display = "none";
+});
+omOsNavContainer.addEventListener("mouseleave", () => {
+  // Hide the omOs nav container
+  omOsNavContainer.style.display = "none";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const homePage = document.getElementById("homePage");
+  homePage.addEventListener("click", function () {
+    window.location.href = "../index.html";
+  });
+});
